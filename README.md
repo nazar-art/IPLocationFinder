1ec0b012dcbc3b5544afb1c7baf6dfc8af91395d
Nazar Lelyak <nazar.lelyak@gmail.com>
2014-09-13 13:52:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Command for creating source code from wsdl file
+> wsimport - d src/main/java -p org.lelyak.demo.webservices -extension -Xnocompile http://www.webservicex.net/geoipservice.asmx?WSDL
+
+#### some ip addresses:
+  
+  * Googgle   -   173.194.113.192  
+  * BBC       -   212.58.244.20
---
+++ src/test/java/org/lelyak/webservices/GeoIPServiceTest.java
@@ -0,0 +1,43 @@
+package org.lelyak.webservices;
+
+import org.apache.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class GeoIPServiceTest {
+
+    private static Logger log = Logger.getLogger(GeoIPServiceTest.class);
+    private GeoIPServiceSoap serviceSoap;
+    private GeoIPService geoIPService;
+    private GeoIP geoIP;
+
+    public static final String UNITED_STATES = "United States";
+    public static final String UNITED_KINGDOM = "United Kingdom";
+
+    @BeforeMethod(groups = "SOAP_GROUP")
+    public void setUp() throws Exception {
+        log.info("GeoIPServiceTest set up called");
+        geoIPService = new GeoIPService();
+        serviceSoap = geoIPService.getGeoIPServiceSoap();
+        log.info("set up finished");
+    }
+
+    @Test(groups = "SOAP_GROUP", description = "check country name for USA and UK")
+    @Parameters( {"googleIpAddress", "bbcIpAddress" })
+    public void testGetGeoIPServiceSoap(String googleIpAddress, String bbcIpAddress) throws Exception {
+        log.info("test testGetGeoIPServiceSoap() started");
+        geoIP = serviceSoap.getGeoIP(googleIpAddress);
+        Assert.assertTrue(geoIP.getCountryName().equalsIgnoreCase(UNITED_STATES), "country code isn't correct for " + UNITED_STATES);
+
+        geoIP = serviceSoap.getGeoIP(bbcIpAddress);
+        Assert.assertTrue(geoIP.getCountryName().equalsIgnoreCase(UNITED_KINGDOM), "country code isn't correct for " + UNITED_KINGDOM);
+        log.info("test testGetGeoIPServiceSoap() runned successful");
+    }
+
+//    @Test
+    public void testGetGeoIPServiceHttpPost() throws Exception {
+
+    }
+}
